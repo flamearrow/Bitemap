3db44671297aaf2f98eb032624a0a52b2e2a4d45
Chen Cen <ccen@twitter.com>
2015-02-15 13:11:21 -0800

    initiaze db schemas and splash screen for loading db

---
+++ app/src/main/java/com/gb/ml/bitemap/DBConnector.java
@@ -0,0 +1,77 @@
+package com.gb.ml.bitemap;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Access database
+ */
+public class DBConnector {
+
+    private static final String SCHEDULES = "SCHEDULES";
+
+    private static final String ALL_FOODTRUCKS = "ALL_FOODTRUCKS";
+
+    private static final String DB_NAME = "foodtruck_db";
+
+    private static DBConnector mInstance;
+
+    private SQLiteDatabase mDb;
+
+    private DBOpenHelper mHelper;
+
+    private static final String TAG = "mlDB";
+
+    private DBConnector(Context context) {
+        Log.d(TAG, "DBConnector created");
+        mHelper = new DBOpenHelper(context, DB_NAME, null, 1);
+    }
+
+    private class DBOpenHelper extends SQLiteOpenHelper {
+
+        public DBOpenHelper(Context context, String name,
+                SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+            Log.d(TAG, "DBOpenHelper created");
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(TAG, "DBOpenHelper.onCreate()");
+            // no foreign key support yet
+            final String createFoodtrucks = "CREATE TABLE IF NOT EXISTS " + ALL_FOODTRUCKS +
+                    " (id INTEGER primary key, name TEXT, category TEXT, category_detail TEXT, "
+                    + "logo TEXT, url TEXT)";
+            final String createSchedules = "CREATE TABLE IF NOT EXISTS " + SCHEDULES +
+                    " (id INTEGER primary key, foodtruck_id INTEGER, start_time TEXT, end_time TEXT, "
+                    + "address TEXT, lat REAL, lng REAL, street_lat REAL, street_lng REAL)";
+            db.execSQL(createFoodtrucks);
+            db.execSQL(createSchedules);
+            Log.d(TAG, "db created");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // nothing
+        }
+    }
+
+    public static synchronized DBConnector getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new DBConnector(context);
+        }
+        return mInstance;
+    }
+
+    public void open() {
+        mDb = mHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (mDb != null) {
+            mDb.close();
+        }
+    }
+}
--- app/src/main/java/com/gb/ml/bitemap/MainActivity.java
+++ app/src/main/java/com/gb/ml/bitemap/MainActivity.java
@@ -1,39 +1,34 @@
 package com.gb.ml.bitemap;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private static final int SPLASH_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, ScheduleActivity.class));
+            }
+        }, SPLASH_DELAY);
+        initializeDB();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    // debug data and insert into
+    private void initializeDB() {
+        DBConnector connector = DBConnector.getInstance(getApplicationContext());
+        connector.open();
     }
 }
